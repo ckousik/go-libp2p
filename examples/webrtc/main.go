7ec50a2f3e79941d6d65328019306a4c91e8b1d0
Chinmay Kousik <chinmaykousik1@gmail.com>
2022-08-26 22:31:23 +0530

    fix race condition in accepting streams

---
+++ examples/webrtc/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	// "time"
+
+	// "github.com/libp2p/go-libp2p/p2p/protocol/identify"
+	"github.com/libp2p/go-libp2p/p2p/protocol/ping"
+
+	// "github.com/libp2p/go-libp2p/p2p/protocol/ping"
+
+	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/host"
+	"github.com/libp2p/go-libp2p-core/peer"
+	webrtc "github.com/libp2p/go-libp2p/p2p/transport/webrtc"
+)
+
+func main() {
+	host := createHost()
+	defer host.Close()
+	fmt.Println("listening on: ", host.Network().ListenAddresses())
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go dialHost(ctx, host)
+
+	fmt.Println("press Ctrl+C to quit")
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGTERM, syscall.SIGINT)
+	<-ch
+	cancel()
+}
+
+func dialHost(ctx context.Context, server host.Host) {
+	client, err := libp2p.New(
+		libp2p.Transport(webrtc.New),
+		libp2p.DisableRelay(),
+		libp2p.Ping(true),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = server.ID().Validate(); err != nil {
+		panic(err)
+	}
+
+	remoteInfo := peer.AddrInfo{
+		ID:    server.ID(),
+		Addrs: server.Network().ListenAddresses(),
+	}
+
+	fmt.Println("=========================== connecting ==============================")
+	err = client.Connect(context.Background(), remoteInfo)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("============================ connected ==============================")
+
+	resultChan := ping.Ping(ctx, server, server.ID())
+
+	for i := 0; i < 5; i++{
+		select {
+		case <-ctx.Done():
+		case result := <-resultChan:
+			if result.Error != nil {
+				fmt.Println("ping error", result.Error)
+			} else {
+				fmt.Println("pinged", remoteInfo.Addrs, " in ", result.RTT)
+			}
+		}
+
+	}
+
+}
+
+func createHost() host.Host {
+	h, err := libp2p.New(
+		libp2p.Transport(webrtc.New),
+		libp2p.ListenAddrStrings(
+			"/ip4/0.0.0.0/udp/0/webrtc", // a UDP endpoint for the WebRTC Transport
+		),
+		libp2p.DisableRelay(),
+		libp2p.Ping(true),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	return h
+}
--- p2p/transport/webrtc/datachannel.go
+++ p2p/transport/webrtc/datachannel.go
@@ -6,10 +6,14 @@ import (
 	"io"
 	"net"
 	"os"
+
 	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/libp2p/go-libp2p-core/network"
+	pb "github.com/libp2p/go-libp2p/p2p/transport/webrtc/pb"
+	protoio "github.com/libp2p/go-msgio/protoio"
 	"github.com/pion/datachannel"
 	"github.com/pion/webrtc/v3"
 )
@@ -26,193 +30,315 @@ type iorequest struct {
 	done  chan ioresponse
 }
 
-const defaultReadBufferLen = 3000
+const defaultReadBufferLen = 8192
 
 // Package pion detached data channel into a net.Conn
 // and then a network.MuxedStream
 type dataChannel struct {
 	// TODO: Are these circular references okay?
 	dc            datachannel.ReadWriteCloser
+	channel       *webrtc.DataChannel
 	laddr         net.Addr
 	raddr         net.Addr
 	closeRead     chan struct{}
 	closeWrite    chan struct{}
 	readDeadline  *deadline
 	writeDeadline *deadline
-	readBuffer    *bytes.Buffer
 	readChan      chan *iorequest
 	ctx           context.Context
 	cancel        context.CancelFunc
 	wg            sync.WaitGroup
+	m             sync.Mutex
+	readBuf       bytes.Buffer
+	wc            atomic.Bool
 }
 
 func newDataChannel(
 	dc datachannel.ReadWriteCloser,
+	channel *webrtc.DataChannel,
 	pc *webrtc.PeerConnection,
 	laddr, raddr net.Addr) *dataChannel {
 	ctx, cancel := context.WithCancel(context.Background())
 
-	var b bytes.Buffer
-	b.Grow(defaultReadBufferLen)
-
 	result := &dataChannel{
 		dc:            dc,
+		channel:       channel,
 		laddr:         laddr,
 		raddr:         raddr,
 		readDeadline:  newDeadline(),
 		writeDeadline: newDeadline(),
 		ctx:           ctx,
 		cancel:        cancel,
-		readBuffer:    &b,
 		readChan:      make(chan *iorequest, 20),
 		closeRead:     make(chan struct{}),
 		closeWrite:    make(chan struct{}),
 	}
 
-	result.wg.Add(1)
-	go result.readLoop()
+	channel.OnClose(func() {
+		log.Errorf("remote closed stream")
+		// result.Close()
+	})
+
+	// channel.OnBufferedAmountLow(func() {
+	// 	log.Infof("[%s] buffered amount low: amount: %d, threshold: %d", channel.Label(), channel.BufferedAmount(), channel.BufferedAmountLowThreshold())
+	// })
+
+	// result.wg.Add(1)
+	// go result.readLoop()
 
 	return result
 }
 
-func (d *dataChannel) readLoop() {
-	defer d.wg.Done()
-	for {
-		select {
-		case <-d.ctx.Done():
-			return
-		case <-d.closeRead:
-			return
-		case request := <-d.readChan:
-			// bad request or readChan is closed
-			if request == nil {
-				return
-			}
-			// test for cancelled read
-			select {
-			case <-request.done:
-				continue
-			default:
-			}
+// func (d *dataChannel) readLoop() {
+// 	defer d.wg.Done()
+// 	rdr := protoio.NewDelimitedReader(d.dc, defaultReadBufferLen)
+// 	for {
+// 		select {
+// 		case <-d.ctx.Done():
+// 			return
+// 		case request := <-d.readChan:
+// 			// bad request or readChan is closed
+// 			if request == nil {
+// 				return
+// 			}
+// 			// test for cancelled read
+// 			select {
+// 			case <-request.done:
+// 				continue
+// 			default:
+// 			}
 
-			// if we have data from a previous successful read, return that data
-			if n, err := d.readBuffer.Read(request.chunk); err != io.EOF {
-				request.done <- ioresponse{n, nil}
-				continue
-			}
-			// read from the datachannel
-			n, err := d.dc.Read(request.chunk)
+// 			var msg pb.Message
+// 			err := rdr.ReadMsg(&msg)
+// 			if err != nil && err != io.EOF {
+// 				log.Errorf("%v", err)
+// 				request.done <- ioresponse{0, err}
+// 				continue
+// 			}
+// 			log.Warnf("flag: %v", msg.Flag)
+// 			if msg.Flag != nil {
+// 				switch msg.GetFlag() {
+// 				case pb.Message_CLOSE_WRITE:
+// 					log.Warnf("remote closed write")
+// 					d.dc.Close()
+// 				case pb.Message_CLOSE_READ:
+// 				case pb.Message_RESET:
+// 					log.Errorf("remote reset the connection")
+// 				}
+// 			}
+// 			if err == io.EOF {
+// 				select {
+// 				case <-request.done:
+// 					continue
+// 				default:
+// 				}
+// 				request.done <- ioresponse {0, err}
+// 				continue
+// 			}
+// 			if msg.Flag == nil && msg.GetMessage() == nil {
+// 				panic("what??")
+// 			}
 
-			select {
-			// the read has been cancelled, so write the data
-			// to the buffer instead
-			case <-request.done:
-				d.readBuffer.Write(request.chunk)
-				continue
-			default:
-			}
-			request.done <- ioresponse{n, err}
-		}
-	}
-}
+// 			select {
+// 			case <-d.closeRead:
+// 				request.done <- ioresponse {0, io.EOF}
+// 				continue
+// 			// the read has been cancelled, so discard
+// 			case <-request.done:
+// 				continue
+// 			default:
+// 			}
+// 			rcv := msg.Message
+// 			if rcv == nil {
+// 				rcv = []byte{}
+// 			}
+// 			copied := copy(request.chunk, rcv)
+// 			request.done <- ioresponse{copied, nil}
+// 		}
+// 	}
+// }
 
 func (d *dataChannel) Read(b []byte) (int, error) {
-	select {
-	case <-d.ctx.Done():
-		return 0, os.ErrClosed
-	case <-d.closeRead:
-		return 0, io.EOF
-	// case where deadline is in the past
-	case <-d.readDeadline.wait():
-		return 0, os.ErrDeadlineExceeded
-	default:
-	}
+	// select {
+	// case <-d.ctx.Done():
+	// 	return 0, os.ErrClosed
+	// case <-d.closeRead:
+	// 	return 0, io.EOF
+	// // case where deadline is in the past
+	// case <-d.readDeadline.wait():
+	// 	return 0, os.ErrDeadlineExceeded
+	// default:
+	// }
 
-	done := make(chan ioresponse)
+	// 	done := make(chan ioresponse)
 
-	// try to queue the read
-	select {
-	case d.readChan <- &iorequest{chunk: b, done: done}:
-	case <-d.ctx.Done():
-		close(done)
-		return 0, os.ErrClosed
-	case <-d.readDeadline.wait():
-		close(done)
-		return 0, os.ErrDeadlineExceeded
-	}
+	// 	// try to queue the read
+	// 	select {
+	// 	case d.readChan <- &iorequest{chunk: b, done: done}:
+	// 	case <-d.ctx.Done():
+	// 		close(done)
+	// 		return 0, os.ErrClosed
+	// 	case <-d.readDeadline.wait():
+	// 		close(done)
+	// 		return 0, os.ErrDeadlineExceeded
+	// 	}
 
-	// wait for read completion and cancel the read
-	// request if it times out or the connection is
-	// closed
-	select {
-	case <-d.ctx.Done():
-		close(done)
-		return 0, os.ErrClosed
-	case <-d.readDeadline.wait():
-		close(done)
-		return 0, os.ErrDeadlineExceeded
-	case result := <-done:
-		return result.int, result.error
+	// 	// wait for read completion and cancel the read
+	// 	// request if it times out or the connection is
+	// 	// closed
+	// 	select {
+	// 	case <-d.ctx.Done():
+	// 		close(done)
+	// 		return 0, os.ErrClosed
+	// 	case <-d.readDeadline.wait():
+	// 		close(done)
+	// 		return 0, os.ErrDeadlineExceeded
+	// 	case result := <-done:
+	// 		// log.Infof("read result: %d %v", result.int, result.error)
+	// 		// log.Debugf("read: %s" ,string(b[:result.int]))
+	// 		return result.int, result.error
+	// 	}
+
+	// method 2
+	initialRead, ierr := d.readBuf.Read(b)
+	if ierr == io.EOF && d.wc.Load() {
+		return initialRead, io.EOF
 	}
+	if initialRead > 0 {
+		return initialRead, nil
+	}
+	var msg pb.Message
+	rdr := protoio.NewDelimitedReader(d.dc, 1500)
+	err := rdr.ReadMsg(&msg)
+	if err != nil {
+		return initialRead, err
+	}
+	copied := copy(b[initialRead:], msg.GetMessage())
+	d.readBuf.Write(msg.GetMessage()[copied:])
+	if msg.Flag != nil && msg.GetFlag() == pb.Message_CLOSE_WRITE {
+		d.wc.Store(true)
+		return copied, nil
+	}
+	return copied, err
 }
+
+// func (d *dataChannel) Read(b []byte) (int, error) {
+// 	for {
+// 		d.bufm.Lock()
+// 		n, err := d.readBuffer.Read(b)
+// 		// log.Infof("reading from buffer: %d %v", n, err)
+// 		d.bufm.Unlock()
+// 		if err == io.EOF && !d.isReadClosed() {
+// 			err = nil
+// 		}
+// 		if n > 0 {
+// 			log.Infof("received %d bytes: %s", n, string(b))
+// 			return n, err
+// 		}
+// 		if n == 0 {
+// 			select {
+// 			case <-d.ctx.Done():
+// 				return 0, io.EOF
+// 			case <-d.closeRead:
+// 				return n, io.EOF
+// 			case <-d.readDeadline.wait():
+// 				return 0, os.ErrDeadlineExceeded
+// 			case <-d.msgChan:
+
+// 			}
+// 		}
+// 	}
+// }
 
 func (d *dataChannel) Write(b []byte) (int, error) {
 	select {
 	case <-d.ctx.Done():
 		return 0, os.ErrClosed
+	case <-d.writeDeadline.wait():
+		return 0, os.ErrDeadlineExceeded
 	case <-d.closeWrite:
 		return 0, os.ErrClosed
 	default:
 	}
 
-	done := make(chan ioresponse)
+	// done := make(chan ioresponse)
 
-	go func() {
-		n, err := d.dc.Write(b)
-		done <- struct {
-			int
-			error
-		}{n, err}
-	}()
+	// go func() {
+	// 	wr := protoio.NewDelimitedWriter(d.dc)
+	// 	err := wr.WriteMsg(
+	// 		&pb.Message{
+	// 			Message: b,
+	// 		})
+	// 	if err != nil {
+	// 		log.Warnf("write error: %v", err)
+	// 	}
+	// 	done <- struct {
+	// 		int
+	// 		error
+	// 	}{len(b), err}
+	// }()
 
-	select {
-	case <-d.ctx.Done():
-		return 0, os.ErrClosed
-	case <-d.writeDeadline.wait():
-		return 0, os.ErrDeadlineExceeded
-	case result := <-done:
-		return result.int, result.error
+	// select {
+	// case <-d.ctx.Done():
+	// 	return 0, os.ErrClosed
+	// case <-d.writeDeadline.wait():
+	// 	return 0, os.ErrDeadlineExceeded
+	// case result := <-done:
+	// 	fmt.Printf("[%s] wrote: %s\n", d.uuid, string(b))
+	// 	return result.int, result.error
 
-	}
+	// }
+
+	// fmt.Printf("[%s][%s] writing: %s\n", d.uuid, d.channel.Label(), string(b))
+	wr := protoio.NewDelimitedWriter(d.dc)
+	_ = wr.WriteMsg(
+		&pb.Message{
+			Message: b,
+		})
+	// fmt.Printf("[%s][%s] wrote: %s\n", d.uuid, d.channel.Label(), string(b))
+	return len(b), nil
 }
 
 func (d *dataChannel) Close() error {
+	select {
+	case <-d.ctx.Done():
+		return nil
+	default:
+	}
+	err := d.channel.Close()
+	d.CloseWrite()
 	d.cancel()
-	_ = d.CloseRead()
-	_ = d.CloseWrite()
-	err := d.dc.Close()
-	d.wg.Wait()
+	// d.wg.Wait()
 	return err
 }
 
 func (d *dataChannel) CloseRead() error {
 	select {
 	case <-d.ctx.Done():
+		return nil
 	case <-d.closeRead:
+		return nil
 	default:
 		close(d.closeRead)
 	}
+
 	return nil
 }
 
 func (d *dataChannel) CloseWrite() error {
 	select {
 	case <-d.ctx.Done():
+		return nil
 	case <-d.closeWrite:
+		return nil
 	default:
 		close(d.closeWrite)
 	}
-	return nil
+	wr := protoio.NewDelimitedWriter(d.dc)
+	err := wr.WriteMsg(&pb.Message{
+		Flag: pb.Message_CLOSE_WRITE.Enum(),
+	})
+	return err
 }
 
 func (d *dataChannel) LocalAddr() net.Addr {
@@ -224,6 +350,7 @@ func (d *dataChannel) RemoteAddr() net.Addr {
 }
 
 func (d *dataChannel) Reset() error {
+	log.Errorf("stream reset")
 	return d.Close()
 }
 
@@ -241,4 +368,13 @@ func (d *dataChannel) SetReadDeadline(t time.Time) error {
 func (d *dataChannel) SetWriteDeadline(t time.Time) error {
 	d.writeDeadline.set(t)
 	return nil
+}
+
+func (d *dataChannel) isReadClosed() bool {
+	select {
+	case <-d.closeRead:
+		return true
+	default:
+	}
+	return false
 }
