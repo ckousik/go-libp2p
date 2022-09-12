894fe7f542a003b2449fc6e41f6f0f43e4d3266c
John Turpish <john@littlebearlabs.io>
2022-09-11 23:45:52 -0400

    Using a file to communicate multiaddr

--- examples/webrtc/main.go
+++ examples/webrtc/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"net"
+	"net/http"
 	"os"
 	"os/signal"
+	"path"
+	"strconv"
 	"syscall"
 
 	"github.com/libp2p/go-libp2p"
@@ -17,6 +21,7 @@ import (
 )
 
 var listenerIp = net.IPv4(0, 0, 0, 0)
+
 func init() {
 	ifaces, err := net.Interfaces()
 	if err != nil {
@@ -46,7 +51,7 @@ func echoHandler(stream network.Stream) {
 	for {
 		reader := bufio.NewReader(stream)
 		str, err := reader.ReadString('\n')
-		if err !=  nil {
+		if err != nil {
 			return
 		}
 		log.Printf("echo: %s", str)
@@ -68,7 +73,27 @@ func main() {
 	}
 
 	remoteAddrs, _ := peer.AddrInfoToP2pAddrs(&remoteInfo)
-	fmt.Println("p2p addr: ", remoteAddrs[0])
+	p2pAddr := remoteAddrs[0]
+	fmt.Println("p2p addr: ", p2pAddr)
+	if len(os.Args) > 1 {
+		http_port, port_err := strconv.ParseUint(os.Args[1], 10, 16)
+		if port_err == nil {
+			go hostMultiAddrOnHttp(p2pAddr.String(), uint16(http_port))
+		} else if _, err := os.Stat(os.Args[1]); err == nil {
+			js_path := path.Join(os.Args[1], "server-multiaddr.js")
+			f, e := os.Create(js_path)
+			if e == nil {
+				f.WriteString("export var SERVER_MULTIADDR = '" + p2pAddr.String() + "';")
+				fmt.Println("Wrote ", js_path)
+			} else {
+				fmt.Println("Was unable to create", js_path)
+				os.Exit(1)
+			}
+		} else {
+			fmt.Println("Your argument", os.Args[1], "does not appear to be a valid port number", port_err)
+			os.Exit(1)
+		}
+	}
 
 	fmt.Println("press Ctrl+C to quit")
 	ch := make(chan os.Signal, 1)
@@ -138,4 +163,14 @@ func createHost() host.Host {
 
 // }
 
-
+func hostMultiAddrOnHttp(ma string, port uint16) {
+	handler := func(w http.ResponseWriter, _ *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		io.WriteString(w, ma)
+		fmt.Println(w)
+	}
+	http.HandleFunc("/listening-multiaddr", handler)
+	la := "127.0.0.1:" + strconv.Itoa(int(port))
+	fmt.Println("Listening for http on ", la)
+	log.Fatal(http.ListenAndServe(la, nil))
+}
